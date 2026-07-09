4f6da6c71111369576c794d8111b3c0f29702b25
Manoj Shinde <123250765+smanoj1215@users.noreply.github.com>
2026-07-09 22:32:56 +0530

    Add current directory context to docker build command

--- vars/build_img.groovy
+++ vars/build_img.groovy
@@ -1,3 +1,3 @@
 def call (String username, String image_name, String tag) {
-    sh "docker build -t ${username}/${image_name}:${tag}"
+    sh "docker build -t ${username}/${image_name}:${tag} ."
 }
