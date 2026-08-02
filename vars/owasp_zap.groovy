4f73d2d7880bd10ea66bac0b36749f0ed7eb31f4
manoj <smanoj.1215@gmail.com>
2026-08-02 05:04:09 +0000

    Added owasp zap

---
+++ vars/owasp_zap.groovy
@@ -0,0 +1,25 @@
+def call(String targetUrl) {
+
+    sh """
+        docker run --rm \
+            --network host \
+            -v \$(pwd):/zap/wrk/:rw \
+            ghcr.io/zaproxy/zaproxy:stable \
+            zap-baseline.py \
+            -t ${targetUrl} \
+            -r zap-report.html \
+            -J zap-report.json \
+            -x zap-report.xml
+    """
+
+    publishHTML(target: [
+        reportDir: '.',
+        reportFiles: 'zap-report.html',
+        reportName: 'OWASP ZAP Report',
+        keepAll: true,
+        alwaysLinkToLastBuild: true,
+        allowMissing: false
+    ])
+
+    archiveArtifacts artifacts: 'zap-report.*', fingerprint: true
+}
